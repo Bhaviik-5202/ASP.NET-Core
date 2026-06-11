622a1d2b1df2acfe69c7c2ee9d38f3a1acc7b646
BhaviikCode-5202 <er.bhavik5202@gmail.com>
2026-06-11 19:37:03 +0530

    Basic Program

---
+++ New Demo Project/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace New_Demo_Project.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
